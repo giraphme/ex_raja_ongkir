de28f7b592445b4c3d1044bb198499aa90ac11ed
TamakiMaeda <giraph.me@gmail.com>
2018-04-22 17:25:26 +0900

    Add dependent packages

--- mix.exs
+++ mix.exs
@@ -11,18 +11,19 @@ defmodule ExRajaOngkir.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:power_assert, "~> 0.1.1", only: :test},
+      {:httpotion, "~> 3.1.0"},
+      {:jason, "~> 1.0"},
+      {:mix_test_watch, "~> 0.5", only: :dev, runtime: false, github: "ignota/mix-test.watch"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 end
