58db36841f0730d282ca0bb9c803aefe1281a379
TamakiMaeda <giraph.me@gmail.com>
2018-04-22 15:49:56 +0900

    :tada: First commit

---
+++ lib/ex_raja_ongkir.ex
@@ -0,0 +1,18 @@
+defmodule ExRajaOngkir do
+  @moduledoc """
+  Documentation for ExRajaOngkir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExRajaOngkir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_raja_ongkir_test.exs
@@ -0,0 +1,8 @@
+defmodule ExRajaOngkirTest do
+  use ExUnit.Case
+  doctest ExRajaOngkir
+
+  test "greets the world" do
+    assert ExRajaOngkir.hello() == :world
+  end
+end
