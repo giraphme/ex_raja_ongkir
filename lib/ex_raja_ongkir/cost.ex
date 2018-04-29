6b74332fbb57d6c1e98609350feb8fa1a378338f
TamakiMaeda <giraph.me@gmail.com>
2018-04-29 13:12:50 +0800

    Support basic and pro plan :tada:

--- lib/ex_raja_ongkir/cost.ex
+++ lib/ex_raja_ongkir/cost.ex
@@ -10,22 +10,80 @@ defmodule ExRajaOngkir.Cost do
     :courier_name,
     :description,
     :service_name,
-    :estimates
+    :estimates,
+    :params
   ]
 
-  def calculate!(from, to, weight, courier) do
-    {:ok, result} = calculate(from, to, weight, courier)
+  def calculate!(from, to, weight, courier, opts \\ []) do
+    {:ok, result} = calculate(from, to, weight, courier, opts)
     result
   end
 
-  def calculate(from, to, weight, courier) when is_list(courier) do
-    calculate(from, to, weight, Enum.join(courier, ":"))
+  def calculate(from, to, weight, courier, opts \\ [])
+
+  def calculate(from, to, weight, courier, opts) when is_list(courier) do
+    calculate(from, to, weight, Enum.join(courier, ":"), opts)
   end
 
-  def calculate(%{id: from_id} = from, %{id: to_id} = to, weight, courier)
+  def calculate(from, to, weight, courier, opts)
       when is_integer(weight) and is_binary(courier) do
-    params = %{origin: from_id, destination: to_id, weight: weight, courier: courier}
+    struct = make_struct(from, to, weight, courier, opts)
 
+    response =
+      struct
+      |> make_params()
+      |> request()
+      |> Request.take_result()
+
+    response
+    |> case do
+      {:ok, results} -> {:ok, response_into(results, struct)}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp make_struct(from, to, weight, courier, opts) do
+    %__MODULE__{
+      params: %{
+        origin: from,
+        destination: to,
+        weight: weight,
+        courier: courier,
+        opts: opts
+      }
+    }
+  end
+
+  defp make_params(%__MODULE__{params: params}) do
+    make_params_by_plan(
+      ExRajaOngkir.plan(),
+      params
+    )
+  end
+
+  defp make_params_by_plan(:starter, %{
+         origin: %{id: origin_id},
+         destination: %{id: destination_id},
+         weight: weight,
+         courier: courier
+       }) do
+    %{origin: origin_id, destination: destination_id, weight: weight, courier: courier}
+  end
+
+  defp make_params_by_plan(:basic, params) do
+    make_params_by_plan(:starter, params)
+  end
+
+  @defult_origin_type 'city'
+  @defult_destination_type 'city'
+  defp make_params_by_plan(:pro, %{opts: opts} = params) do
+    Map.merge(make_params_by_plan(:basic, params), %{
+      originType: opts[:origin_type] || @defult_origin_type,
+      destinationType: opts[:destination_type] || @defult_destination_type
+    })
+  end
+
+  defp request(params) do
     Request.post(
       "cost",
       headers: ["Content-Type": "application/x-www-form-urlencoded"],
@@ -34,29 +92,24 @@ defmodule ExRajaOngkir.Cost do
         |> Enum.map(fn {key, value} -> "#{key}=#{value}" end)
         |> Enum.join("&")
     )
-    |> Request.take_result()
-    |> case do
-      {:ok, results} -> {:ok, parse_to_self(from, to, weight, results)}
-      {:error, reason} -> {:error, reason}
-    end
   end
 
-  defp parse_to_self(from, to, weight, results) do
-    results
-    |> Enum.map(fn result ->
-      base_struct = %__MODULE__{
-        from: from,
-        to: to,
-        weight: weight,
-        # TODO
-        weight_unit: 'g',
-        courier_code: result["code"],
-        courier_name: result["name"]
+  defp response_into(response, %__MODULE__{params: params} = struct) do
+    response
+    |> Enum.map(fn row ->
+      base_struct = %{
+        struct
+        | from: params[:from],
+          to: params[:destination],
+          weight: params[:weight],
+          weight_unit: 'g',
+          courier_code: row["code"],
+          courier_name: row["name"]
       }
 
       {
         base_struct.courier_code |> String.to_atom(),
-        Enum.map(result["costs"], fn cost ->
+        Enum.map(row["costs"], fn cost ->
           %{
             base_struct
             | description: cost["description"],
--- test/ex_raja_ongkir/cost_test.exs
+++ test/ex_raja_ongkir/cost_test.exs
@@ -2,17 +2,32 @@ defmodule ExRajaOngkir.CostTest do
   use ExUnit.Case
 
   describe "ExRajaOngkir.Cost.calculate/4" do
-    # TODO
-    # test "It return array of %City{}"
+    @plans [
+      starter: %{},
+      basic: %{courier: ~w(jne tiki)},
+      pro: %{courier: ~w(jne tiki)},
+      pro: %{courier: ~w(jne tiki), origin_type: "city", destination_type: "city"}
+    ]
 
-    test "It return %Cost{}" do
-      city_1 = %ExRajaOngkir.City{id: "1"}
-      city_2 = %ExRajaOngkir.City{id: "2"}
-      weight = 17_000
-      courier = "jne"
+    Enum.each(@plans, fn {plan, params} ->
+      @plan plan
+      @params params
 
-      assert {:ok, %{jne: [%ExRajaOngkir.Cost{}]}} =
-               ExRajaOngkir.Cost.calculate(city_1, city_2, weight, courier)
-    end
+      test "It return %Cost{} when plan is #{@plan} and the params is #{inspect(@params)}" do
+        default_plan = Application.get_env(:ex_raja_ongkir, :plan)
+        Application.put_env(:ex_raja_ongkir, :plan, @plan)
+
+        from = @params[:from] || %ExRajaOngkir.City{id: "1"}
+        to = @params[:to] || %ExRajaOngkir.City{id: "2"}
+        weight = @params[:weight] || 17_000
+        courier = @params[:courier] || "jne"
+        opts = @params[:opts] || []
+
+        assert {:ok, %{jne: [%ExRajaOngkir.Cost{}]}} =
+                 ExRajaOngkir.Cost.calculate(from, to, weight, courier, opts)
+
+        Application.put_env(:ex_raja_ongkir, :plan, default_plan)
+      end
+    end)
   end
 end
